73588b3db59c4ff02c4ca111527bb882b3fe0f59
enigma <camilo.murcia.pimentel@gmail.com>
2022-01-27 13:18:17 -0500

    feat: added login token authentication button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const CLIENT_ID = "4d1e166ef5ec4a6caad3ffd242454083";
+const REDIRECT_URL = "http://localhost:3000/";
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+const RESPONSE_TYPE = "token";
+
+const App = () => {
+  
+	const [token, setToken] = useState("");
+
+	// extract token from url hash and update it.
+	useEffect(() => {
+		const hash = window.location.hash;
+		let token =window.localStorage.getItem("token");
+		
+		if(!token && hash) {
+			token = hash.substring().split("&").find(e => e.startsWith("#access_token")).split("=")[1];
+			window.location.hash="";
+			window.localStorage.setItem("token", token);
+		}
+
+		setToken(token);
+	}, []);
+	
+	
+	return (
+  		<div>
+			<div>
+				<a 
+					href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&response_type=${RESPONSE_TYPE}`}
+					className='btn btn-primary'
+					role="button"
+				>
+					Let's get started
+				</a>
+			</div>
+    	</div>
+  )
+};
+
 
 export default App;
